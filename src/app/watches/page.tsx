c01aaa59245376903502baf4b0621ab3220e9afc
zaheem hassan <164178436+zaheem55@users.noreply.github.com>
2024-11-19 21:26:31 +0500

    Update page.tsx

--- src/app/watches/page.tsx
+++ src/app/watches/page.tsx
@@ -1,15 +1,15 @@
-import { describe } from 'node:test'
 import React from 'react'
+import Image from 'next/image'
 
 const Watches = () => {
 
 const watchData = [
-  { id: 1, name: 'Rolex', price: 16000, description: 'Luxury Watch', image: '/rolex.webp'},
-  { id: 2, name: 'Omega', price: 9000, description: 'Luxury Watch', image: '/omega.webp'},
-  { id: 3, name: 'Patek Philippe', price: 25000, description: 'Luxury Watch', image: '/patek-philippe.jpg'},
-  { id: 4, name: 'TAG Heuer', price: 6000, description: 'Luxury Watch', image: '/tag-heuer.jpg'},
-  { id: 5, name: 'Breiting', price: 5000, description: 'Luxury Watch', image: '/breiting.jpg'},
-  { id: 6, name: 'Seiko', price: 2000, description: 'Luxury Watch', image: '/seiko.jpg'},
+  { id: 1, name: 'Rolex', price: 16000, description: 'Luxury Watch', Image: '/rolex.webp'},
+  { id: 2, name: 'Omega', price: 9000, description: 'Luxury Watch', Image: '/omega.webp'},
+  { id: 3, name: 'Patek Philippe', price: 25000, description: 'Luxury Watch', Image: '/patek-philippe.jpg'},
+  { id: 4, name: 'TAG Heuer', price: 6000, description: 'Luxury Watch', Image: '/tag-heuer.jpg'},
+  { id: 5, name: 'Breiting', price: 5000, description: 'Luxury Watch', Image: '/breiting.jpg'},
+  { id: 6, name: 'Seiko', price: 2000, description: 'Luxury Watch', Image: '/seiko.jpg'},
 ]
 
   return (
@@ -17,7 +17,7 @@ const watchData = [
       <div className='watches'>
       {watchData.map((watch) => (
         <div key={watch.id} className="watch-card">
-          <img  src={watch.image} alt={watch.name}/>
+          <Image  src={watch.Image} alt={watch.name}/>
           <h3>{watch.name}</h3>
           <p>{watch.description}</p>
           <div className="price">${watch.price}</div>
